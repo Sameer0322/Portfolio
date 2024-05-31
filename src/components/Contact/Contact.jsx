f1376a840289d89c6e0758cfa06450d8106815ce
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2024-05-31 22:39:29 +0530

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="www.linkedin.com/in/sameer-saxena-428215218">Sameer Saxena</a>
+          <a href="https://www.linkedin.com/in/sameer-saxena-428215218">Sameer Saxena</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
