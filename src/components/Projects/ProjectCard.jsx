d042b6ced30635881f82aa43bd36362e63827a28
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2024-06-01 10:03:32 +0530

    Update Target

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -24,7 +24,7 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href={source} className={styles.link}>
+        <a href={source} className={styles.link} target="blank">
           Source
         </a>
       </div>
