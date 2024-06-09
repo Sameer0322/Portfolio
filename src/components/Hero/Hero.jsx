61f916d0b367d8c1192278c8ee2f47d6a35aad14
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2024-06-09 20:44:33 +0530

    Update Small Screen Css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typewriter } from 'react-simple-typewriter';
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 830px)');
+    const handleMediaQueryChange = (event) => {
+      setIsSmallScreen(event.matches);
+    };
+
+    // Set initial state
+    setIsSmallScreen(mediaQuery.matches);
+
+    // Listen for media query changes
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Cleanup listener on component unmount
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Sameer</h1>
         <div className={styles.description}>
-          <Typewriter
-            words={['A dedicated Web Developer passionate about building engaging websites, with a keen interest in Python programming. While proficient in web technologies, I am intent on improving my skills in Data Structures and Algorithms to enhance my problem-solving abilities.']}
-            loop={1} // Change this to `0` or `true` for infinite loop
-            cursor
-            cursorStyle='_'
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
+          {isSmallScreen ? (
+            <span>
+              A dedicated Web Developer passionate about building engaging websites, with a keen interest in Python programming. While proficient in web technologies, I am intent on improving my skills in Data Structures and Algorithms to enhance my problem-solving abilities.
+            </span>
+          ) : (
+            <Typewriter
+              words={['A dedicated Web Developer passionate about building engaging websites, with a keen interest in Python programming. While proficient in web technologies, I am intent on improving my skills in Data Structures and Algorithms to enhance my problem-solving abilities.']}
+              loop={1}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          )}
         </div>
         {/* <a href="https://drive.google.com/file/d/1YQP-n1mAANHo1giTkGK2vHU9YEFApvLO/view?usp=sharing" className={styles.contactBtn} target="_blank">
           Resume
